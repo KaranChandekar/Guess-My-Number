3f1b51cc6dce7d5e4e7eedb14a0c6a98e3f5d374
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-14 18:16:41 +0530

    grab elements

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const checkBtn = document.querySelector('.check');
+const guessInput = document.querySelector('.guess');
+const again = document.querySelector('.again');
+const numberEl = document.querySelector('.number');
+const scoreEl = document.querySelector('.score');
+const highscoreEl = document.querySelector('.highscore');
+const messageEl = document.querySelector('.message');
+const body = document.querySelector('body');
