173ffcd099caab8fc265136624eef0513ae53a07
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-14 18:18:13 +0530

    add functionality to checkbtn

--- script.js
+++ script.js
@@ -8,3 +8,36 @@ const scoreEl = document.querySelector('.score');
 const highscoreEl = document.querySelector('.highscore');
 const messageEl = document.querySelector('.message');
 const body = document.querySelector('body');
+
+checkBtn.addEventListener('click', function () {
+  const guess = Number(guessInput.value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔ No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    numberEl.textContent = secretNumber;
+    body.style.backgroundColor = '#60b347';
+    numberEl.style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      highscoreEl.textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      scoreEl.textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      scoreEl.textContent = 0;
+    }
+  }
+});
